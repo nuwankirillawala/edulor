b3842b272ecd8785d832d3769c249364c71bdef0
Nuwan Kirillawala <nuwan.sv.kirillawala@gmail.com>
2022-10-25 16:42:14 +0530

    FAQs Section added

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ window.addEventListener('scroll', () => {
         'nav-window-scroll', window.scrollY > 0
     )
 });
+
+//show and hide faqs
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', ()=> {
+        faq.classList.toggle('open');
+
+        //change icon of faq
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = "uil uil-minus";
+        }
+        else{
+            icon.className = "uil uil-plus";
+        }
+    })
+})
